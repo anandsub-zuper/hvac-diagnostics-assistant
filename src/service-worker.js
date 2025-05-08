ae29b5ea5c3d9a0131afc9476b60eb364cfa1b91
anandsub-zuper <anandsub@zuper.co>
2025-05-07 18:02:19 -0700

    Update service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -5,6 +5,8 @@
 // You can also remove this file if you'd prefer not to use a
 // service worker, and the Workbox build step will be skipped.
 
+/* eslint-disable no-restricted-globals */
+
 import { clientsClaim } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
